ba2a3b24e2c6c9fb57cadea88088c3ac383bef10
Attila Róbert Kovács <kovacs.attila.robert@dszcberegszaszi.hu>
2022-05-24 20:32:08 +0200

    Pontosítás

--- orai/Formazas/Program.cs
+++ orai/Formazas/Program.cs
@@ -16,7 +16,7 @@ namespace Formazas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TextFormat());
         }
     }
 }
